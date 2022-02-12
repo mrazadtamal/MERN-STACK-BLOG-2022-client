d4125b7f129fb747de88dab71703a283d8597b3e
mrazadtamal <mrazadt@gmail.com>
2022-02-12 19:33:48 +0600

    app

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Navbar from "./Navbar";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="home">
+      <Navbar />
     </div>
   );
 };
