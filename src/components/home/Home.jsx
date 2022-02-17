4a5080a75d3733128498575a3a096a98866d9fc8
mrazadtamal <mrazadt@gmail.com>
2022-02-17 22:59:01 +0600

    sidebar start

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -5,6 +5,32 @@ const Home = () => {
   return (
     <div className="home">
       <Navbar />
+      <div className="main-content">
+        <div className="container">
+          <div className="row">
+            {/* col - 8  */}
+            <div className="col-8">
+              ----------- ----- ----------- col 8 ------------ -------
+            </div>
+            {/* col-4 */}
+            <div className="col-4">
+              <div className="search-category-tag">
+                <div className="search">
+                  <h1>Search</h1>
+                  <div className="form-group">
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="search"
+                    />
+                    <button className="btn btn-block">Search</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
